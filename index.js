031d608fd3b00403c794d9f4d60be8fc9602496c
Gabriel Lu <gabrielulu@gmail.com>
2020-01-14 15:01:57 +0800

    Working example to collect both available and unavailable units

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
-puppeteer.launch().then(async browser => {
+puppeteer.launch({headless: true})
+.then(async browser => {
   const page = await browser.newPage();
   const toggleAvailability = async (availability) => {
     const option = availability ? '1' : '2'
@@ -19,13 +20,19 @@ puppeteer.launch().then(async browser => {
   let currentURL = 'https://services2.hdb.gov.sg/webapp/BP13AWFlatAvail/BP13SEstateSummary?sel=BTO';
   page.goto(currentURL);
   let unavailableUnits = {}
+  let availableUnits = {}
 
   const collectUnits = async (is_available) => {
     const key = is_available ? 'Available' : 'Taken'
+    console.log('entered data collection', key)
+    await page.waitForSelector('#blockDetails > div:nth-child(1) > table > tbody > tr > td')
     const blockCells = await page.evaluate(() => {
+      console.log('evaluating length', document.querySelectorAll('#blockDetails > div:nth-child(1) > table > tbody > tr > td').length)
       return document.querySelectorAll('#blockDetails > div:nth-child(1) > table > tbody > tr > td').length
     })
+    console.log('blockCells is', blockCells)
     for (let i = 0; i < blockCells; i ++) {
+      console.log("block awaiting")
       await page.waitForSelector('#blockDetails > div:nth-child(1) > table > tbody > tr > td')
       console.log("block appeared")
       reliablyClick('#blockDetails > div:nth-child(1) > table > tbody > tr > td', i)
@@ -34,17 +41,24 @@ puppeteer.launch().then(async browser => {
       console.log("waiting for units")
       const shortlistedUnits = await page.evaluate((key) => {
         const blockNo = document.querySelector('#blockDetails > div:nth-child(2) > div.large-3.columns').textContent.trim()
-        const rows = [...document.querySelectorAll('#blockDetails > div:nth-child(6) > table > tbody > tr')]
+        const rows = key === 'Available' ? 
+        [...document.querySelectorAll('#blockDetails > div:nth-child(6) > table > tbody > tr > td > font > a > font:nth-child(1)')] 
+        : [...document.querySelectorAll('#blockDetails > div:nth-child(6) > table > tbody > tr > td')]
         let output = []
-        rows.forEach(row => Array.from(row.children).forEach(cell => output.push(cell.textContent.trim())))
+        rows.forEach(row => output.push(row.textContent.trim()))
         return {
           [blockNo]: {
             [key]: output
           }
         }
       },key)
-      unavailableUnits = {...unavailableUnits,...shortlistedUnits}
+      if (key === 'Available') {
+        availableUnits = {...availableUnits,...shortlistedUnits}
+      } else {
+        unavailableUnits = {...unavailableUnits,...shortlistedUnits}
+      }
       console.log(unavailableUnits)
+      console.log(availableUnits)
     }
   }
   try {
@@ -53,15 +67,17 @@ puppeteer.launch().then(async browser => {
     reliablyClick("tr[id*='Punggol'][id*='Room'] > td > a")
     await page.waitForSelector('#ViewOption')
 
-    toggleAvailability(false)
-    collectUnits(false)
-    // toggleAvailability(true)
-    // collectUnits(true)
+    await toggleAvailability(false)
+    await collectUnits(false)
+
+    await toggleAvailability(true)
+    await collectUnits(true)
   } catch (e) {
     console.log(e)
     if (e instanceof puppeteer.errors.TimeoutError) {
       // Do something if this is a timeout.
     }
   }
-});
+})
+.catch(e => console.log(e));
 
